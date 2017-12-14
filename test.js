6f5316a66fcfac295414118ca391deb049745818
TH1485 <34238688+TH1485@users.noreply.github.com>
2017-12-14 15:52:18 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-Tado= require('./client.js')
+json = require
+Tado = require('./client.js')
 var client = new Tado();
 var login='';
 var pwd='';
@@ -6,6 +7,7 @@ client.login(login, pwd).then((success) => {
   console.log("login: " + success);
   return client.api('/me').then((result) => {
     console.log('me',result);
+    console.log("homeid: " + JSON.parse(result).homes[0].id);
   });
 }).catch( (error) => {
   console.log("Error:" + error);
