49fb500654073f7d8a8ddc1fedb7cf42bcf74673
TH1485 <34238688+TH1485@users.noreply.github.com>
2017-12-14 14:25:34 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,135 +1,128 @@
-'use strict';
+'use strict'
 
-module.exports= class Client {
+module.exports =  function () {
+
+    const request = require('request');
+    const moment = require('moment');
+
+    const BASE_URL = 'https://my.tado.com';
+    const AUTH_URL = 'https://auth.tado.com';
+    const CLIENT_ID = 'tado-web-app';
+    const CLIENT_SECRET = 'wZaRN7rpjn3FoNyF5IFuxg9uMzYJcvOoQ8QWiIqS3hfk6gLhVlG57j5YNoZL2Rtc';
+    const REFERER = 'https://my.tado.com/';
     
-  const request = require('request');
-  const moment = require('moment');
+ 
+    class Client {
 
-  const BASE_URL = 'https://my.tado.com';
-  const AUTH_URL = 'https://auth.tado.com';
-  const CLIENT_ID = 'tado-web-app';
-  const CLIENT_SECRET = 'wZaRN7rpjn3FoNyF5IFuxg9uMzYJcvOoQ8QWiIqS3hfk6gLhVlG57j5YNoZL2Rtc';
-  const REFERER = 'https://my.tado.com/';
+      display() {
+        console.log(request);
+        console.log(AUTH_URL);
+        }
 
-  constructor (username,password) {
-    this.username=username;
-    this.password=password;
-    }
-  
-  display() {
-    console.log(this.username);
-    }
-  
-  login() {
-      return new Promise((resolve, reject) => {
-        console.log(this.password); 
-        request.post({
-          url: AUTH_URL + '/oauth/token',
-          qs: {
-              client_id: CLIENT_ID,
-              client_secret: CLIENT_SECRET,
-              grant_type: 'password',
-              password: this.password,
-              username: this.username,
-              scope: 'home.user'
-            },
-            json: true
-          }, (err, response, result) => {
-            if (err || response.statusCode !== 200) {
-              reject(err || result);
-            } else {
-              this.saveToken(result);
-              resolve(true);
-            }
-        });
-      });
-  }
-
-  saveToken(token) {
-      this.token = token;
-      this.token.expires_in = moment().add(token.expires_in, 'seconds').toDate();
-  }
-
-  refreshToken() {
-      return new Promise((resolve, reject) => {
-          if (!this.token) {
-              return reject(new Error('not logged in'));
-          }
-
-          if (moment().subtract(5, 'seconds').isBefore(this.token.expires_in)) {
-              return resolve();
-          }
-
-          request.get({
-              url: AUTH_URL + '/oauth/token',
-              qs: {
-                  client_id: CLIENT_ID,
-                  grant_type: 'refresh_token',
-                  refresh_token: this.token.refresh_token
-              },
-              json: true
-          }, (err, response, result) => {
-              if (err || response.statusCode !== 200) {
+      login(username,password) {
+          return new Promise((resolve, reject) => {
+              request.post({
+                  url: AUTH_URL + '/oauth/token',
+                  qs: {
+                      client_id: CLIENT_ID,
+                      client_secret: CLIENT_SECRET,
+                      grant_type: 'password',
+                      password: password,
+                      username: username,
+                      scope: 'home.user'
+                    },
+                    json: true
+              }, (err, response, result) => {
+                if (err || response.statusCode !== 200) {
                   reject(err || result);
-              } else {
+                } else {
                   this.saveToken(result);
                   resolve(true);
-              }
+                }
+            });
           });
-      });
-  }
+      }
 
-  api(path) {
-      return this.refreshToken()
-          .then(() => {
-              return new Promise((resolve, reject) => {
-                  request.get({
-                      url: BASE_URL + '/api/v2' + path,
-                      json: true,
-                      headers: {
-                          referer: REFERER
-                      },
-                      auth: {
-                          bearer: this.token.access_token
-                      }
-                  }, (err, response, result) => {
-                      if (err || response.statusCode !== 200) {
-                          reject(err || result);
-                      } else {
-                          resolve(result);
-                      }
-                  });
+      saveToken(token) {
+          this.token = token;
+          this.token.expires_in = moment().add(token.expires_in, 'seconds').toDate();
+      }
+
+      refreshToken() {
+          return new Promise((resolve, reject) => {
+              if (!this.token) {
+                  return reject(new Error('not logged in'));
+              }
+
+              if (moment().subtract(5, 'seconds').isBefore(this.token.expires_in)) {
+                  return resolve();
+              }
+
+              request.get({
+                  url: AUTH_URL + '/oauth/token',
+                  qs: {
+                      client_id: CLIENT_ID,
+                      grant_type: 'refresh_token',
+                      refresh_token: this.token.refresh_token
+                  },
+                  json: true
+              }, (err, response, result) => {
+                  if (err || response.statusCode !== 200) {
+                      reject(err || result);
+                  } else {
+                      this.saveToken(result);
+                      resolve(true);
+                  }
               });
           });
-  }
+      }
 
-  me() {
-      return this.api('/me');
-  }
+      api(path) {
+          return this.refreshToken()
+              .then(() => {
+                  return new Promise((resolve, reject) => {
+                      request.get({
+                          url: BASE_URL + '/api/v2' + path,
+                          json: true,
+                          headers: {
+                              referer: REFERER
+                          },
+                          auth: {
+                              bearer: this.token.access_token
+                          }
+                      }, (err, response, result) => {
+                          if (err || response.statusCode !== 200) {
+                              reject(err || result);
+                          } else {
+                              resolve(result);
+                          }
+                      });
+                  });
+              });
+      }
 
-  home(homeId) {
-      return this.api('/homes/${homeId}');
-  }
+      me() {
+          return this.api('/me');
+      }
 
-  zones(homeId) {
-      return this.api('/homes/${homeId}/zones');
-  }
+      home(homeId) {
+          return this.api('/homes/${homeId}');
+      }
 
-  weather(homeId) {
-      return this.api('/homes/${homeId}/weather');
-  }
+      zones(homeId) {
+          return this.api('/homes/${homeId}/zones');
+      }
 
-  state(homeId, zoneId) {
-      return this.api('/homes/${homeId}/zones/${zoneId}/state' );
-  }
+      weather(homeId) {
+          return this.api('/homes/${homeId}/weather');
+      }
+
+      state(homeId, zoneId) {
+          return this.api('/homes/${homeId}/zones/${zoneId}/state' );
+      }
 
 
+    }
+   
+    return new Client();
 }
-    
-module.exports.Client
-
-               
-               
-
-                      
-                  
