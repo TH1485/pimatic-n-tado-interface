1687cc90dd508df0abe952d8ecfe7c9190175fdb
TH1485 <34238688+TH1485@users.noreply.github.com>
2017-12-14 11:18:45 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const Tado = require('./client.js');
 
-let client = new Tado();
-client.login('username', 'password').then((success) => {
+let client = new Tado(username,password);
+client.login().then((success) => {
   client.api('/me').then((result) => {
     console.log('me', result);
   });
